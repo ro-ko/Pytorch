f10531d8a63523a1cc99eb4ed252b483cdc3b4c0
roko <201719987@jbnu.ac.kr>
2022-12-31 12:01:34 +0900

    update

--- DL_model/test.py
+++ DL_model/test.py
@@ -1,10 +1,21 @@
-import torchvision.models.vgg as vgg
-import torch.nn as nn
-import torch
-from typing import Any, cast, Dict, List, Union
-import VGG
+import numpy as np
+
+def sigmoid(x):
+    x = 1/(1+np.exp(x))
+    return x
+t = np.array([[1],[1],[2],[1],[-1]])
+a = np.array([[1,1,1,1],[1,0,0,0],[1,0,0,0],[1,1,1,1]])
+c = np.zeros((5,4))
+r = np.array([[0.5],[1.2],[0.3],[0.7],[-1],[9]])
+print(sigmoid(np.dot(c,t[0:-1])+t[-1]).shape)
+print(r.round().shape)
 
 
 
-test = VGG.vgg11(num_classes=1000,dropout=0.5)
-print(test)
+e = np.zeros_like(t)
+
+e[0:-1] = t[0:-1]
+print(e)
+print(e[0:-1].shape, t[0:-1].shape)
+
+print(np.random.randn(1,2))
